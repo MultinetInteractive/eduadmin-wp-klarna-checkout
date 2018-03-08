ebea549ee022d8fa8db115c6bc3b5fb6734d2f9c
Chris Gårdenberg <chga@multinet.se>
2018-03-08 14:04:48 +0100

    Adding standard plugin file with header required by WordPress

---
+++ eduadmin-wp-klarna-checkout.php
@@ -0,0 +1,30 @@
+<?php
+defined( 'ABSPATH' ) || die( 'This plugin must be run within the scope of WordPress.' );
+
+/*
+ * Plugin Name: EduAdmin Booking - Klarna Checkout-plugin
+ * Plugin URI:  https://www.eduadmin.se
+ * Description: Plugin to EduAdmin Booking to enable Klarna Checkout-integration
+ * Version:     1.0.0
+ * GitHub Plugin Uri: https://github.com/MultinetInteractive/eduadmin-wp-klarna-checkout
+ * Requires at least: 3.0
+ * Tested up to: 4.9
+ * Author: Chris Gårdenberg, MultiNet Interactive AB
+ * Author URI: https://www.multinet.com
+ * License: GPL3
+ * Text Domain: eduadmin-wp-klarna-checkout
+ */
+/*
+	EduAdmin Booking plugin
+	Copyright (C) 2015-2017 Chris Gårdenberg, MultiNet Interactive AB
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
